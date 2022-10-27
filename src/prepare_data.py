e9de9ce6587f84e5c6c56f392de1ddda155d8349
grgau <pedro.ferracini@usp.br>
2022-10-27 20:15:36 -0300

    Initial prepare_data structure

---
+++ src/prepare_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+class Preprocess:
+    def __init__(self):
+        None
+
+    def get_data():
+        return
+
+    def preprocess_data():
+        return
+
+    def split_features_and_target():
+        return
+
+    def perform(self, data: pd.DataFrame) -> pd.DataFrame:
+        return
