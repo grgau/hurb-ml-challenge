8dd7fa76a6e7858de298948ec63f6de0b5544722
grgau <pedro.ferracini@usp.br>
2022-10-29 00:36:44 -0300

    Add catboost model

---
+++ src/models/catboost_clf.py
@@ -0,0 +1,25 @@
+import os
+import mlflow
+
+from .base_model import BaseModel
+from catboost import CatBoostClassifier
+
+class CatBoostClf(BaseModel):
+    def __init__(self, artifact: str = 'catboost'):
+        super(CatBoostClf, self).__init__()
+        self.model = CatBoostClassifier(iterations=100)
+        self.artifact = artifact
+
+    def log_model(self):
+        mlflow.set_tracking_uri(os.getenv("MLFLOW_URL", ""))
+
+        with mlflow.start_run(run_name=self.run_name) as run:
+            mlflow.log_metric('train_accuracy', self.train_accuracy)
+            mlflow.log_metric('test_accuracy', self.test_accuracy)
+            mlflow.log_metric('train_recall', self.train_recall)
+            mlflow.log_metric('test_recall', self.test_recall)
+            mlflow.log_metric('train_precision', self.train_precision)
+            mlflow.log_metric('test_precision', self.test_precision)
+
+            mlflow.log_params(self.model.get_all_params()) # Log all model's params
+            mlflow.catboost.log_model(self.model, artifact_path=self.artifact)
