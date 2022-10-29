b06f4526e77640100e0f35fcb51551e13df86fe0
grgau <pedro.ferracini@usp.br>
2022-10-29 00:34:44 -0300

    Serve model with json input and output

--- src/bento_service.py
+++ src/bento_service.py
@@ -1,12 +1,23 @@
-import numpy as np
 import bentoml
-from bentoml.io import NumpyNdarray
+import numpy as np
+import pandas as pd
+from bentoml.io import JSON
+from .prepare_data import Preprocess
 
-iris_clf_runner = bentoml.sklearn.get("iris_clf:latest").to_runner()
+hotel_reservation_clf_runner = bentoml.catboost.get("hotel_reservation_clf:latest").to_runner()
 
-svc = bentoml.Service("iris_classifier", runners=[iris_clf_runner])
+svc = bentoml.Service("hotel_reservation_classifier", runners=[hotel_reservation_clf_runner])
 
-@svc.api(input=NumpyNdarray(), output=NumpyNdarray())
+@svc.api(input=JSON(), output=JSON())
 def classify(input_series: np.ndarray) -> np.ndarray:
-    result = iris_clf_runner.predict.run(input_series)
-    return result
+    try:
+        X, _ = Preprocess.preprocess_data(pd.json_normalize(input_series))
+        result = hotel_reservation_clf_runner.predict.run(X)
+
+        print("Result", X, result)
+        if result[0] == 1:
+            return { "will_cancel": True }
+        else:
+            return{ "will_cancel": False }
+    except Exception as e:
+        return { "error": str(e) }
