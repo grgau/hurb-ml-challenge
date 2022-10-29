0c934ba29f0e38a53e469e1ce403c030ac56b3a5
grgau <pedro.ferracini@usp.br>
2022-10-29 00:35:02 -0300

    Log metrics of performance and parameters of catboost to mlflow

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -1,4 +1,5 @@
 import time
+from sklearn.metrics import accuracy_score, recall_score, precision_score
 
 class BaseModel:
     def __init__(self, artifact: str = 'base-model'):
@@ -13,8 +14,20 @@ class BaseModel:
         self.train_precision = None
         self.test_precision = None
 
-    def train(self, X, y, log_to_mlflow: bool = True):
-        self.model.fit(X, y)
+    def train_test(self, X_train, y_train, X_test, y_test, log_to_mlflow: bool = True):
+        self.model.fit(X_train, y_train)
+
+        y_train_pred = self.model.predict(X_train)
+
+        self.train_accuracy = accuracy_score(y_train, y_train_pred)
+        self.train_recall = recall_score(y_train, y_train_pred)
+        self.train_precision = precision_score(y_train, y_train_pred)
+
+        y_test_pred = self.model.predict(X_test)
+
+        self.test_accuracy = accuracy_score(y_test, y_test_pred)
+        self.test_recall = recall_score(y_test, y_test_pred)
+        self.test_precision = precision_score(y_test, y_test_pred)
 
         if log_to_mlflow:
             self.log_model()
